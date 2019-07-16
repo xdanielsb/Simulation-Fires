6eb94f729901fa0c203f68828aaf38986702bf49
Daniel Santos <dfsantosbu@unal.edu.co>
2019-07-15 19:27:51 -0500

    Create script to hide elements that cause fire.

--- view/script/index.js
+++ view/script/index.js
@@ -1,2 +1,26 @@
 'use strict'
-//$('.ui.sidebar').sidebar('toggle');
+
+$(document).ready(function () {
+  //$('.ui.sidebar').sidebar('toggle');
+  $('#chkCigar').change(function () {
+    if($('#chkCigar').is(':checked')){
+      $('#img-cigar').show();
+    }else{
+      $('#img-cigar').hide();
+    }
+  });
+  $('#chkOutlet').change(function () {
+    if($('#chkOutlet').is(':checked')){
+      $('#img-toma').show();
+    }else{
+      $('#img-toma').hide();
+    }
+  });
+  $('#chkVela').change(function () {
+    if($('#chkVela').is(':checked')){
+      $('#img-vela').show();
+    }else{
+      $('#img-vela').hide();
+    }
+  });
+});
