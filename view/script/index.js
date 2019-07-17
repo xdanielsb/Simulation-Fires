bc7ea09cf2aba8fb02e5887e8053ca6dd858d8e8
Daniel Santos <dfsantosbu@unal.edu.co>
2019-07-17 11:23:22 -0500

    Implement logic to stop the simulation.
    Add extra details.

--- view/script/index.js
+++ view/script/index.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
   //$('.ui.sidebar').sidebar('toggle');
   $('#fire').hide();
   $('.fog__container').hide();
+  $('#detenerSimulacion').hide();
+  $('#iniciarGraficasSimulacion').hide();
+
   $('#chkCigar').change(function () {
     if($('#chkCigar').is(':checked')){
       $('.container-cigar').show();
@@ -33,6 +36,11 @@ $(document).ready(function () {
   $('#iniciarSimulacion').click(function(){
     $(".smokeElement").addClass("smoke");
     $("#isOk").addClass("animate");
+    let canStartFire = $('#chkVela').is(':checked') || $('#chkOutlet').is(':checked') || $('#chkCigar').is(':checked');
+    if( !canStartFire ) return;
+    $('#iniciarSimulacion').hide();
+    $('#detenerSimulacion').show();
+    $('#iniciarGraficasSimulacion').show();
     setTimeout( function(){
       $(".content-body").addClass("fog");
       $('.fog__container').show();
@@ -46,5 +54,16 @@ $(document).ready(function () {
       startFire();
     }  , oneSecond * 20 );
   })
-
+  $('#detenerSimulacion').click(function(){
+    $('#detenerSimulacion').hide();
+    $('#iniciarSimulacion').show();
+    $("#isOk").removeClass("animate");
+    $("#isSmoke").removeClass("animate");
+    $("#isFire").removeClass("animate");
+    $(".smokeElement").removeClass("smoke");
+    $(".content-body").removeClass("fog");
+    $('.fog__container').hide();
+    $('#fire').hide();
+    $('#iniciarGraficasSimulacion').hide();
+  })
 });
