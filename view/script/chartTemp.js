61ca95303332850cad50c951fdbeba461e5f47bf
Daniel Santos <dfsantosbu@unal.edu.co>
2019-07-17 12:38:59 -0500

    IMplement code for simulate smoke.

--- view/script/chartTemp.js
+++ view/script/chartTemp.js
@@ -1,110 +1,142 @@
 var lastDate = 0;
-       var data = []
-       var TICKINTERVAL = 86400000
-       let XAXISRANGE = 777600000
-       function getDayWiseTimeSeries(baseval, count, yrange) {
-           var i = 0;
-           while (i < count) {
-               var x = baseval;
-               var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+var data = [];
 
-               data.push({
-                   x, y
-               });
-               lastDate = baseval
-               baseval += TICKINTERVAL;
-               i++;
+console.log(data);
+
+var options = {
+   chart: {
+       height: 250,
+       type: 'line',
+       animations: {
+           enabled: true,
+           easing: 'linear',
+           dynamicAnimation: {
+               speed: 10
            }
+       },
+       toolbar: {
+           show: false
+       },
+       zoom: {
+           enabled: false
        }
+   },
+   dataLabels: {
+       enabled: false
+   },
+   stroke: {
+       curve: 'smooth'
+   },
+   series: [{
+       data: data
+   }],
+   title: {
+       text: 'Temperatura Actual',
+       align: 'left'
+   },
+   markers: {
+       size: 0
+   },
+   xaxis: {
+       type: 'category',
+       min: 0,
+       range: 100,
+       labels:{
+         show: false,
+       }
+   },
+   yaxis: {
+       min: 0,
+       max: 100
+   },
+   legend: {
+       show: false
+   },
+}
 
-       getDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 10, {
-           min: 10,
-           max: 90
-       })
 
-       function getNewSeries(baseval, yrange) {
-           var newDate = baseval + TICKINTERVAL;
-           lastDate = newDate
-
-           for(var i = 0; i< data.length - 10; i++) {
-               // IMPORTANT
-               // we reset the x and y of the data which is out of drawing area
-               // to prevent memory leaks
-               data[i].x = newDate - XAXISRANGE - TICKINTERVAL
-               data[i].y = 0
+var optionsH = {
+   chart: {
+       height: 250,
+       type: 'line',
+       animations: {
+           enabled: true,
+           easing: 'linear',
+           dynamicAnimation: {
+               speed: 10
            }
-
-           data.push({
-               x: newDate,
-               y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
-           })
-
+       },
+       toolbar: {
+           show: false
+       },
+       zoom: {
+           enabled: false
        }
-
-       function resetData(){
-           // Alternatively, you can also reset the data at certain intervals to prevent creating a huge series
-           data = data.slice(data.length - 10, data.length);
+   },
+   dataLabels: {
+       enabled: false
+   },
+   stroke: {
+       curve: 'smooth'
+   },
+   series: [{
+       data: data
+   }],
+   title: {
+       text: 'Humo Actual',
+       align: 'left'
+   },
+   markers: {
+       size: 0
+   },
+   xaxis: {
+       type: 'category',
+       min: 0,
+       range: 100,
+       labels:{
+         show: false,
        }
+   },
+   yaxis: {
+       min: 0,
+       max: 100
+   },
+   legend: {
+       show: false
+   },
+}
 
-       var options = {
-           chart: {
-               height: 350,
-               type: 'line',
-               animations: {
-                   enabled: true,
-                   easing: 'linear',
-                   dynamicAnimation: {
-                       speed: 1000
-                   }
-               },
-               toolbar: {
-                   show: false
-               },
-               zoom: {
-                   enabled: false
-               }
-           },
-           dataLabels: {
-               enabled: false
-           },
-           stroke: {
-               curve: 'smooth'
-           },
-           series: [{
-               data: data
-           }],
-           title: {
-               text: 'Dynamic Updating Chart',
-               align: 'left'
-           },
-           markers: {
-               size: 0
-           },
-           xaxis: {
-               type: 'datetime',
-               range: XAXISRANGE,
-           },
-           yaxis: {
-               max: 100
-           },
-           legend: {
-               show: false
-           },
-       }
+var chart = new ApexCharts(
+   document.querySelector("#chartT"),
+   options
+);
 
-       var chart = new ApexCharts(
-           document.querySelector("#chart"),
-           options
-       );
 
-       chart.render();
+var chartH = new ApexCharts(
+   document.querySelector("#chartH"),
+   optionsH
+);
 
-       window.setInterval(function () {
-           getNewSeries(lastDate, {
-               min: 10,
-               max: 90
-           })
-           chart.updateSeries([{
-               data: data
-           }])
-       }, 1000)
+chart.render();
+chartH.render();
+
+var initX=0, initY=17;
+window.setInterval(function () {
+
+   for(var i = 0; i< data.length -10; i++) {
+     data[i].x  =0;
+     data[i].y = 0;
+   }
+   var y = initY;
+   var x = initX;
+   data.push({x, y});
+   initY =  initY + 1;
+   initX =  initX + 10;
+   chart.updateSeries([{
+       data: data
+   }])
+   chartH.updateSeries([{
+       data: data
+   }])
+
+}, 1000)
